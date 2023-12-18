7cbf2947c1354022f825666ae1fb84a857d722a4
Albert Andrews <03jayteee@gmail.com>
2023-12-18 09:18:56 +0000

    Ecounter and patient files

---
+++ models/Encounter.js
@@ -0,0 +1,13 @@
+// models/encounter.js
+const mongoose = require('mongoose');
+
+const encounterSchema = new mongoose.Schema({
+  patientID: String,
+  date: Date,
+  time: String,
+  type: String,
+});
+
+const Encounter = mongoose.model('Encounter', encounterSchema);
+
+module.exports = Encounter;
---
+++ models/Patient.js
@@ -0,0 +1,20 @@
+// models/patient.js
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+  patientID: String,
+  surname: String,
+  otherNames: String,
+  gender: String,
+  phoneNumber: String,
+  residentialAddress: String,
+  emergencyContact: {
+    name: String,
+    contact: String,
+    relationship: String,
+  },
+});
+
+const Patient = mongoose.model('Patient', patientSchema);
+
+module.exports = Patient;
